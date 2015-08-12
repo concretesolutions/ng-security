216498ba12f1486e948f67a8b13bb10fe955f9af
patrick.porto <patrick.porto@concretesolutions.com.br>
2015-08-12 17:23:12 -0300

    Add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-var gulp = require('gulp');
+'use strict';
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+  return gulp.src('./src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('test', ['lint'], function () {
+
+});
