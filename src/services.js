733110a3f385603ffa7e87fc137615f8bf8de1db
patrick.porto <patrick.porto@concretesolutions.com.br>
2015-08-14 18:47:32 -0300

    Add login by web service

--- src/services.js
+++ src/services.js
@@ -2,12 +2,13 @@ angular
   .module('ngSecurity')
   .factory('$security', securityFactory);
 
-securityFactory.$inject = ['$cookies'];
+securityFactory.$inject = ['$cookies', '$q', '$http'];
 
-function securityFactory ($cookies) {
+function securityFactory ($cookies, $q, $http) {
   /** interface */
   var security = {
     login: login,
+    loginByUrl: loginByUrl,
     logout: logout
   };
 
@@ -17,6 +18,15 @@ function securityFactory ($cookies) {
   function login (token, user) {
     $cookies.put('ng-security-authorization', token);
     $cookies.putObject('ng-security-user', user);
+  }
+
+  function loginByUrl (url, data) {
+    return $q(function (resolve, reject) {
+      $http.post(url, data).success(function (data) {
+        security.login(data.token, data.user);
+        resolve(data);
+      }).error(reject);
+    });
   }
 
   function logout () {
--- tests/security-service-spec.js
+++ tests/security-service-spec.js
@@ -3,11 +3,26 @@
 describe('Service:security', function () {
   beforeEach(module('ngSecurity'));
   var $cookies,
-      $security;
+      $security,
+      $httpBackend;
 
   beforeEach(inject(function ($injector) {
     $cookies = $injector.get('$cookies');
     $security = $injector.get('$security');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.whenPOST('/api/auth', {
+      username: 'admin',
+      password: 'admin'
+    }).respond(201, {
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+      user: {
+        name: 'Patrick Porto'
+      }
+    });
+
+    $cookies.remove('ng-security-authorization');
+    $cookies.remove('ng-security-user');
   }));
 
   it('should authenticate without user object', function () {
@@ -21,6 +36,18 @@ describe('Service:security', function () {
       name: 'Patrick Porto'
     });
 
+    assert.equal($cookies.getObject('ng-security-user').name, 'Patrick Porto');
+  });
+
+  it('should authenticate by web service', function () {
+    $security.loginByUrl('/api/auth', {
+      username: 'admin',
+      password: 'admin'
+    });
+
+    $httpBackend.flush();
+
+    assert.equal($cookies.get('ng-security-authorization'), 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
     assert.equal($cookies.getObject('ng-security-user').name, 'Patrick Porto');
   });
 
