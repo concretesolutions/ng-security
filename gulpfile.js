1921cc5c3778502a5d60cbdca99527f06722d8a8
patrick.porto <patrick.porto@concretesolutions.com.br>
2015-08-14 15:10:47 -0300

    Add wrapper

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
     closureCompiler = require('gulp-closure-compiler'),
+    wrapper = require('gulp-wrapper'),
     karma = require('karma').server,
     config = require('./package.json');
 
@@ -24,10 +25,17 @@ gulp.task('test', ['lint'], function (done) {
 gulp.task('dist', function () {
   return gulp.src('./src/*.js')
     .pipe(concat(config.name + '.js'))
+    .pipe(wrapper({
+      header: '"use strict";\n(function () {\n',
+      footer: '}).call(window);'
+    }))
     .pipe(gulp.dest('dist'))
     .pipe(closureCompiler({
       compilerPath: 'node_modules/closure-compiler/lib/vendor/compiler.jar',
-      fileName: config.name + '.min.js'
+      fileName: config.name + '.min.js',
+      compilerFlags: {
+        language_in: 'ECMASCRIPT5_STRICT'
+      }
     }))
     .pipe(gulp.dest('dist'));
 });
--- src/services.js
+++ src/services.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular
   .module('ngSecurity')
   .factory('$security', securityFactory);
